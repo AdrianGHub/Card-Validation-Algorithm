40d3760046f5e97b5561dd243aee44e75ad02711
Adrian <adrian_zinko@op.pl>
2020-03-14 10:23:39 +0100

    feat: declare variable for regex replacement

--- src/app.js
+++ src/app.js
@@ -4,21 +4,19 @@ import checkCardNumber from './card-checker';
 // console.log(cardChecker('4111111111111111'));
 
 
-
-const form = document.querySelector('form');
 const cardNameInput = document.querySelector('.card-number');
-const submitBtn = document.querySelector('.submit-btn');
 const errorEl = document.querySelector('.error');
+const regexReplace = cardNameInput.value.replace(/\s+/g, ''); 
 
 document.forms[0].addEventListener('submit', (e) => {
     e.preventDefault();
 
     switch(true) {
-        case cardNameInput.value.replace(/\s+/g, '').length < 13 :
+        case regexReplace.length < 13 :
             errorEl.classList.add('red')
             errorEl.innerText = 'Numer karty musi zawierać od 13 do 16 cyfr';
             break;
-        case cardNameInput.value.replace(/\s+/g, '').length > 16 :
+        case regexReplace.length > 16 :
             errorEl.classList.add('red');
             errorEl.innerText = 'Numer karty musi zawierać od 13 do 16 cyfr';
             break;
