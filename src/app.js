7b7d2862abd099fd31dbbbe6771aea93cc7b7322
Adrian <adrian_zinko@op.pl>
2020-03-14 15:58:56 +0100

    feat: add proper validation message style

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-import checkCardNumber from './card-checker';
+import checkCardNumber from './cardChecker';
 
 
 // console.log(cardChecker('4111111111111111'));
 
 
-const cardNameInput = document.querySelector('.card-number');
+const cardNameInput = document.querySelector('.card_input');
 const errorEl = document.querySelector('.error');
 let regexReplace = cardNameInput.value.replace(/\s+/g, ''); 
 
@@ -13,20 +13,22 @@ document.forms[0].addEventListener('submit', (e) => {
 
     switch(true) {
         case cardNameInput.value.replace(/\s+/g, '').length < 13 :
-            errorEl.classList.add('red')
+            errorEl.classList.add('error')
             errorEl.innerText = 'Numer karty nie może zawierać poniżej 13 cyfr';
             setInterval(() => errorEl.innerText = '', 5000);
             break;
         case cardNameInput.value.replace(/\s+/g, '').length > 16 :
-            errorEl.classList.add('red');
+            errorEl.classList.add('error');
             errorEl.innerText = 'Numer karty nie może zawierać powyżej 16 cyfr';
             setInterval(() => errorEl.innerText = '', 5000);
         break;
         default: 
-            errorEl.classList.remove('red');
+            errorEl.classList.remove('error');
             errorEl.innerText = '';
     }
     
     checkCardNumber(cardNameInput.value);
 
 })
+
+cardNameInput.addEventListener('click', (e) => e.target.value = '');
